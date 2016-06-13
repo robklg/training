112f7da363064d67a1af4d8248220df3d836eff5
HashiCorp Training <training@hashicorp.com>
2016-06-13 10:15:50 +0000

    whatever

---
+++ dns.tf
@@ -0,0 +1,28 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your DNSimple email is:
+#
+#     sethvargo+terraform@gmail.com
+#
+# Your DNSimple token is:
+#
+#     1Gam3SE2eIwZYtq70H5V5iAXiE9sGPmf
+#
+# Your Identity is:
+#
+#     hashiconf-37693cfc748049e45d87b8c7d8b9aacd
+#
+
+provider "dnsimple" {
+  token = "1Gam3SE2eIwZYtq70H5V5iAXiE9sGPmf"
+  email = "sethvargo+terraform@gmail.com"
+}
+
+resource "dnsimple_record" "myrecord" {
+  domain = "terraform.rocks"
+  name = "robklg"
+  value = "${aws_instance.web.0.public_ip}"
+  type = "A"
+  ttl = 3600
+}
---
+++ main.tf
@@ -0,0 +1,62 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-99a58be2
+#
+# Your security group ID is:
+#
+#     sg-8e08dfe6
+#
+# Your AMI ID is:
+#
+#     ami-74ee001b
+#
+# Your Identity is:
+#
+#     hashiconf-37693cfc748049e45d87b8c7d8b9aacd
+#
+# Your AccessKeyID and SecretAccessKey are:
+#
+#     AKIAICQ3HPJA54G3WBJA
+#     cpm7MTswNI1VxNEPw0Ojt4uwUdOGUf8aAFhB7Fey
+#
+
+variable "access_key" {} 
+variable "secret_key" {}
+variable "region" {
+  default = "eu-central-1"
+}
+
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-74ee001b"
+  instance_type = "t2.micro"
+
+  subnet_id = "subnet-99a58be2"
+  vpc_security_group_ids = [ "sg-8e08dfe6" ]
+
+  count = "2"
+
+  tags {
+    Identity = "hashiconf-37693cfc748049e45d87b8c7d8b9aacd"
+    Owner = "Rob klein Gunnewiek"
+    Organization = "Bol.com"
+  }
+}
+
+output "public_ip" {
+  value = "${join(", ", aws_instance.web.*.public_ip)}"
+}
+
+output "public_dns" {
+  value = "${join(", ", aws_instance.web.*.public_dns)}"
+}
+
